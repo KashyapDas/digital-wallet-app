8ee5447a01539741dd7b7115b757b077e73449e9
Kashyap Das <kashyap.education2234@gmail.com>
2024-11-16 12:11:20 +0530

    final backend commit

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,19 @@ const app = express();
 const userRouter = require("./routes/user"); 
 const accountRouter = require("./routes/account");
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Add your frontend URL(s)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+    credentials: true, // This allows cookies/authentication
+    maxAge: 86400 // Cache preflight request results for 24 hours (in seconds)
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.options('*', cors(corsOptions));
 
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/account",accountRouter);
